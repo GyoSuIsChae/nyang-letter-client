d5ffe4688f4e267ec3d922e62563ed6b2bcfe9ee
chlee1001 <chlee1001@naver.com>
2022-09-16 23:25:08 +0900

    :wrench: prettier 수정 및 적용

--- src/pages/home-page/index.tsx
+++ src/pages/home-page/index.tsx
@@ -1,12 +1,12 @@
-import React, { FC } from 'react'
-import styles from './home-page.module.css'
+import React from 'react';
+import styles from './home-page.module.css';
 
-const Home: FC = () => {
+const Home = () => {
   return (
     <div className={styles.container}>
       <p>Home</p>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
