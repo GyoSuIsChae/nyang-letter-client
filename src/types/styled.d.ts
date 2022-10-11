7463789173ce55abab863912ac9afb86c8c67a1a
uncoolclub <azxq1000@gmail.com>
2022-10-11 23:41:29 +0900

    :lipstick: styled-components 의 선언 파일 생성

---
+++ src/types/styled.d.ts
@@ -0,0 +1,34 @@
+import { CSSProp } from 'styled-components';
+
+import {
+  FONT_SIZES_TYPE,
+  FONT_WEIGHTS_TYPE,
+  COLORS_TYPE,
+  Z_INDEX_LAYER_TYPE,
+} from '@constants/theme';
+
+declare module 'styled-components' {
+  declare module 'react' {
+    interface Attributes {
+      css?: CSSProp | CSSObject;
+    }
+  }
+
+  export interface DefaultTheme {
+    fontSizes: {
+      [key in FONT_SIZES_TYPE]: number;
+    };
+
+    fontWeights: {
+      [key in FONT_WEIGHTS_TYPE]: number;
+    };
+
+    colors: {
+      [key in COLORS_TYPE]: string;
+    };
+
+    zIndexLayer: {
+      [key in Z_INDEX_LAYER_TYPE]: number;
+    };
+  }
+}
