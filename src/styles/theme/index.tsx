2bde5f9ced1bc28197301583941c4fa56da1308c
uncoolclub <azxq1000@gmail.com>
2022-10-11 23:45:33 +0900

    :lipstick: theme 설정

---
+++ src/styles/theme/index.tsx
@@ -0,0 +1,17 @@
+import { DefaultTheme } from 'styled-components';
+
+import {
+  FONT_SIZES,
+  FONT_WEIGHTS,
+  COLORS,
+  Z_INDEX_LAYER,
+} from '@constants/theme';
+
+const theme: DefaultTheme = {
+  fontSizes: FONT_SIZES,
+  fontWeights: FONT_WEIGHTS,
+  colors: COLORS,
+  zIndexLayer: Z_INDEX_LAYER,
+};
+
+export default theme;
