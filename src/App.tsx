3b641f5ad2c0c21f8415fc9769cbb1fb74361a22
chlee1001 <chlee1001@naver.com>
2022-10-13 01:40:13 +0900

    :art: NLT-55 모달테스트 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import HomePage from '@/pages/homePage';
+import HomePage from '@pages/homePage';
+import ModalTestPage from '@pages/ModalTestPage';
 
 const App = () => (
   <Routes>
     <Route path="/" element={<HomePage />} />
+    <Route path="/modal" element={<ModalTestPage />} />
   </Routes>
 );
 
