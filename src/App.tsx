0ace8b30d66291e50d154827540f5341850bb7ca
chlee1001 <chlee1001@naver.com>
2022-10-30 18:46:42 +0900

    :necktie: NLT-66 공통버튼 테스트 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
+import ButtonTestPage from '@pages/ButtonTestPage';
 import HomePage from '@pages/Home';
 import ModalTestPage from '@pages/ModalTestPage';
 
@@ -8,6 +9,7 @@ const App = () => (
   <Routes>
     <Route path="/" element={<HomePage />} />
     <Route path="/modal" element={<ModalTestPage />} />
+    <Route path="/button" element={<ButtonTestPage />} />
   </Routes>
 );
 
