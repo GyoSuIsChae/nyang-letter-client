cedf7ae4fcb1d3942b4e77c052724f53e07a1bf3
uncoolclub <azxq1000@gmail.com>
2022-10-31 10:17:26 +0900

    :wrench: ErrorMessage 로직 수정

--- src/components/Input/ErrorMessage.tsx
+++ src/components/Input/ErrorMessage.tsx
@@ -11,12 +11,20 @@ interface IErrorMessageProps {
 }
 
 const ErrorBlock = styled.div`
-  ${TextCSS.Body3}
+  ${TextCSS.Body1}
 
-  color: ${({ theme }) => theme.colors.red};
+  width: 100%;
+  display: flex;
+  align-items: flex-start;
+
+  opacity: 0.35;
+  color: ${({ theme }) => theme.colors.red001};
 `;
 
-const ErrorMessage: React.FC<IErrorMessageProps> = ({ id, errors }) => {
+const ErrorMessage: React.FC<IErrorMessageProps> = ({
+  id,
+  errors = undefined,
+}) => {
   if (!errors || !errors?.[id]) {
     return null;
   }
@@ -26,10 +34,6 @@ const ErrorMessage: React.FC<IErrorMessageProps> = ({ id, errors }) => {
       {errors?.[id]?.type === 'maxLength' && '15자 이내로 입력해 주세요.'}
     </ErrorBlock>
   );
-};
-
-ErrorMessage.defaultProps = {
-  errors: undefined,
 };
 
 export default ErrorMessage;
