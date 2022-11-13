723a8e8831c2154e77944f7925dece0835fa87fa
uncoolclub <azxq1000@gmail.com>
2022-11-13 22:11:33 +0900

    :wrench: 경로 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
+import { PATH_NAMES } from '@constants/pages';
 import HomePage from '@pages/Home';
 import ModalTestPage from '@pages/ModalTestPage';
+import OnBoarding from '@pages/OnBoarding';
 
-const App = () => (
-  <Routes>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/modal" element={<ModalTestPage />} />
-  </Routes>
-);
+const App: React.FC = () => {
+  const accessToken = false;
+
+  return (
+    <Routes>
+      <Route
+        path={PATH_NAMES.HOME}
+        element={
+          accessToken ? (
+            <HomePage />
+          ) : (
+            <Navigate to={PATH_NAMES.ON_BOARDING} replace />
+          )
+        }
+      />
+      <Route path={PATH_NAMES.ON_BOARDING} element={<OnBoarding />} />
+      <Route path="/modal" element={<ModalTestPage />} />
+    </Routes>
+  );
+};
 
 export default App;
