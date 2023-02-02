bc759356368e72a683cc00b12160f4533827151f
chlee1001 <chlee1001@naver.com>
2023-02-03 00:11:57 +0900

    :sparkles: NLT-71 달력 컴포넌트 추가

--- src/components/CommonDatePicker/index.tsx
+++ src/components/CommonDatePicker/index.tsx
@@ -1,51 +1,59 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
-/* eslint-disable react/prop-types  */
-/* eslint-disable react/display-name  */
-/* eslint-disable @typescript-eslint/no-unsafe-call  */
-import React, { forwardRef, useState } from 'react';
-import DatePicker, { registerLocale } from 'react-datepicker';
+import React, { PropsWithChildren, useCallback, useState } from 'react';
+import DatePicker, {
+  CalendarContainerProps,
+  registerLocale,
+} from 'react-datepicker';
 
 import 'react-datepicker/dist/react-datepicker.css';
 import { format } from 'date-fns';
 import ko from 'date-fns/locale/ko';
 import styled from 'styled-components';
 
+import arrowDoubleUpIco from '@assets/images/arrow-double-up.png';
 import arrowUpIco from '@assets/images/arrow-up-outline.png';
 import calendarIco from '@assets/images/calendar-outline.png';
 import { TX } from '@components/Text';
+import { COLORS } from '@constants/theme';
+import './styles.css';
 
 registerLocale('ko', ko);
 
 interface CalenderProps {
   data: {
     date: Date;
+    decreaseYear(): void;
     decreaseMonth(): void;
+    increaseYear(): void;
     increaseMonth(): void;
     prevMonthButtonDisabled: boolean;
     nextMonthButtonDisabled: boolean;
   };
 }
 
-const CalendarContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
+const CalendarFooter = styled.div`
   width: 100%;
-  &.react-datepicker__month-container {
-    width: 100%;
-  }
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  gap: 12px;
+  padding: 12px;
 `;
-const CustomCalendar = ({ children }) => {
-  console.log('CustomCalendar', children);
-  return (
-    <CalendarContainer className="react-datepicker__month-container">
-      {children}
-    </CalendarContainer>
-  );
-};
 
-const SubtitleText = styled(TX.SubTitle1)``;
+const StyledButton = styled.button<{
+  confirm?: boolean;
+}>`
+  background-color: ${({ confirm }) =>
+    confirm ? COLORS.black001 : COLORS.white_grey001};
+  color: ${({ confirm }) => (confirm ? COLORS.white_grey001 : COLORS.black001)};
+  padding-block: 12px;
+  width: 100%;
+  border-radius: 6px;
+`;
+
+const SubtitleText = styled(TX.SubTitle1)`
+  font-size: 0.9rem;
+`;
 
 const BodyText = styled(TX.Body1)`
   text-align: center;
@@ -71,16 +79,81 @@ const CalendarIcon = styled.img.attrs({
 const ArrowUpIcon = styled.img.attrs({
   src: arrowUpIco,
   alt: 'arrow-up',
-})`
-  width: 1.2rem;
-  height: 1.2rem;
+})<{
+  isOpen?: boolean;
+  isRight?: boolean;
+  isLeft?: boolean;
+}>`
+  width: 0.8rem;
+  height: 0.8rem;
   ${({ isOpen }) => (!isOpen ? 'transform: rotate(180deg);' : '')}
   ${({ isRight }) => (isRight ? 'transform: rotate(90deg);' : '')}
   ${({ isLeft }) => (isLeft ? 'transform: rotate(-90deg);' : '')}
 `;
 
+const ArrowDoubleUpIcon = styled.img.attrs({
+  src: arrowDoubleUpIco,
+  alt: 'arrow-double-up',
+})<{
+  isRight?: boolean;
+  isLeft?: boolean;
+}>`
+  width: 0.8rem;
+  height: 0.8rem;
+  ${({ isRight }) => (isRight ? 'transform: rotate(90deg);' : '')}
+  ${({ isLeft }) => (isLeft ? 'transform: rotate(-90deg);' : '')}
+`;
+
+const StyledArrowButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 8px;
+`;
+const StyledArrowButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.35rem;
+  border: 1px solid ${COLORS.grey001};
+  border-radius: 0.4rem;
+`;
+
+const StyledDatePickerContainer = styled.div`
+  border: 1px solid #fff;
+  border-radius: 6px;
+  margin-block: 8px;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.15);
+`;
+
+const StyledDatePickerHeader = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-inline: 4px;
+  padding-bottom: 8px;
+`;
+
+const CustomInput = ({
+  value,
+  onClick,
+  isOpen,
+}: {
+  value: Date;
+  onClick: (e: React.MouseEvent<HTMLElement>) => void;
+  isOpen: boolean;
+}) => (
+  <CalenderInput onClick={onClick}>
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <CalendarIcon style={{ marginRight: 8 }} />
+      <BodyText>{format(value, 'yyyy.MM.dd')}</BodyText>
+    </div>
+    <ArrowUpIcon isOpen={isOpen} style={{ marginLeft: 8 }} />
+  </CalenderInput>
+);
+
 const CustomHeader = ({ data }: CalenderProps) => {
-  console.log('CustomHeader', data);
   const formatDate = (d: Date): string => {
     const date = new Date(d);
     const monthIndex = date.getMonth() + 1;
@@ -88,69 +161,78 @@ const CustomHeader = ({ data }: CalenderProps) => {
     return `${year}년 ${`0${monthIndex}`.slice(-2)}월`;
   };
   return (
-    <div
-      style={{
-        width: '100%',
-        backgroundColor: 'red',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        paddingInline: 16,
-        paddingBlock: 8,
-      }}
-    >
-      <button
-        onClick={() => {
-          data.decreaseYear();
-        }}
-      >
-        <ArrowUpIcon isLeft />
-      </button>
-      <button
-        onClick={() => {
-          data.decreaseMonth();
-        }}
-      >
-        <ArrowUpIcon isLeft />
-      </button>
+    <StyledDatePickerHeader>
+      <StyledArrowButtonContainer>
+        <StyledArrowButton onClick={data.decreaseYear}>
+          <ArrowDoubleUpIcon isLeft />
+        </StyledArrowButton>
+        <StyledArrowButton onClick={data.decreaseMonth}>
+          <ArrowUpIcon isLeft />
+        </StyledArrowButton>
+      </StyledArrowButtonContainer>
       <SubtitleText>{formatDate(data.date)}</SubtitleText>
-      <button
-        onClick={() => {
-          data.increaseMonth();
-        }}
-      >
-        <ArrowUpIcon isRight />
-      </button>
-      <button
-        onClick={() => {
-          data.increaseYear();
-        }}
-      >
-        <ArrowUpIcon isRight />
-      </button>
-    </div>
+      <StyledArrowButtonContainer>
+        <StyledArrowButton onClick={data.increaseMonth}>
+          <ArrowUpIcon isRight />
+        </StyledArrowButton>
+        <StyledArrowButton onClick={data.increaseYear}>
+          <ArrowDoubleUpIcon isRight />
+        </StyledArrowButton>
+      </StyledArrowButtonContainer>
+    </StyledDatePickerHeader>
   );
 };
 
-const CustomInput = ({ value, onClick, isOpen }) => {
-  return (
-    <CalenderInput onClick={onClick}>
-      <div style={{ display: 'flex', alignItems: 'center' }}>
-        <CalendarIcon style={{ marginRight: 8 }} />
-        <BodyText>{format(value, 'yyyy.MM.dd')}</BodyText>
-      </div>
-      <ArrowUpIcon isOpen={isOpen} style={{ marginLeft: 8 }} />
-    </CalenderInput>
-  );
-};
+const CustomCalendar = ({
+  children = null,
+  cancelButton,
+  confirmButton,
+}: PropsWithChildren<{
+  cancelButton: () => void;
+  confirmButton: () => void;
+}>) => (
+  <>
+    {children}
+    <CalendarFooter>
+      <StyledButton type="button" onClick={cancelButton}>
+        취소
+      </StyledButton>
+      <StyledButton type="button" confirm onClick={confirmButton}>
+        확인
+      </StyledButton>
+    </CalendarFooter>
+  </>
+);
+
 const CommonDatePicker = () => {
-  const [startDate, setStartDate] = useState(new Date());
+  const initialDate = new Date();
+  const [startDate, setStartDate] = useState<Date>(initialDate);
   const [isCalendarOpen, setIsCalendarOpen] = useState<boolean>(false);
 
-  const handleClick = (e) => {
+  const handleClick = (e: React.MouseEvent<HTMLElement>) => {
     e.preventDefault();
     setIsCalendarOpen((prev) => !prev);
   };
+
+  const onClickToCancel = () => {
+    setIsCalendarOpen(false);
+    setStartDate(initialDate);
+  };
+  const onClickToConfirm = () => {
+    setIsCalendarOpen(false);
+  };
+
+  const renderCalendarContainer = useCallback(
+    (props: CalendarContainerProps) => (
+      <CustomCalendar
+        {...props}
+        cancelButton={() => onClickToCancel()}
+        confirmButton={() => onClickToConfirm()}
+      />
+    ),
+    [],
+  );
+
   return (
     <>
       <CustomInput
@@ -158,17 +240,22 @@ const CommonDatePicker = () => {
         onClick={handleClick}
         isOpen={isCalendarOpen}
       />
+
       {isCalendarOpen && (
-        <DatePicker
-          dateFormat="yyyy.MM.dd"
-          locale="ko"
-          selected={startDate}
-          disabledKeyboardNavigation
-          onChange={(date: Date) => setStartDate(date)}
-          calendarContainer={CustomCalendar}
-          renderCustomHeader={(data) => <CustomHeader data={data} />}
-          inline
-        />
+        <StyledDatePickerContainer>
+          <DatePicker
+            className="customDatePickerWidth"
+            dateFormat="yyyy.MM.dd"
+            locale="ko"
+            selected={startDate}
+            disabledKeyboardNavigation
+            onChange={(date: Date) => setStartDate(date)}
+            calendarContainer={renderCalendarContainer}
+            renderCustomHeader={(data) => <CustomHeader data={data} />}
+            inline
+            minDate={initialDate}
+          />
+        </StyledDatePickerContainer>
       )}
     </>
   );
