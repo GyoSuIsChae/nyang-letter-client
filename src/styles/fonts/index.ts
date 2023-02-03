678b664e69c4f9fac1dcf14b93d9e15e579caa75
uncoolclub <azxq1000@gmail.com>
2023-02-04 00:39:24 +0900

    :lipstick: 이서윤체 추가

--- src/styles/fonts/index.ts
+++ src/styles/fonts/index.ts
@@ -1,9 +1,17 @@
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
+import LeeSeoyunOTF from '@assets/fonts/LeeSeoyun.otf';
 import NanumSquareRoundOTFB from '@assets/fonts/NanumSquareRoundOTFB.otf';
 import NanumSquareRoundOTFR from '@assets/fonts/NanumSquareRoundOTFR.otf';
 import NotoSansKRRegular from '@assets/fonts/NotoSansKRRegular.otf';
 
 export default `
+  @font-face {
+    font-family: 'LeeSeoyun';
+    font-weight: 400;
+    font-style: normal;
+    src: url(${LeeSeoyunOTF}) format('otf');
+  }
+
   @font-face {
     font-family: 'NanumSquareRoundOTFR';
     font-weight: 400;
