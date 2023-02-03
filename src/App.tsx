9e6e20a70d6fbe5f3d4818232815a7c2d79dcfc6
uncoolclub <azxq1000@gmail.com>
2023-02-04 00:54:49 +0900

    :wrench: App 에 레이아웃 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
+import Layout from '@components/Layout';
 import ButtonTestPage from '@pages/ButtonTestPage';
 import HomePage from '@pages/Home';
 import ModalTestPage from '@pages/ModalTestPage';
 
-const App = () => (
-  <Routes>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/modal" element={<ModalTestPage />} />
-    <Route path="/button" element={<ButtonTestPage />} />
-  </Routes>
-);
+const App: React.FC = () => {
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/modal" element={<ModalTestPage />} />
+        <Route path="/button" element={<ButtonTestPage />} />
+      </Route>
+    </Routes>
+  );
+};
 
 export default App;
