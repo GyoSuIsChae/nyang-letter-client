a8150e4e0898614a9248ed41a03af9c6a29fe09d
uncoolclub <azxq1000@gmail.com>
2023-02-04 00:38:32 +0900

    :lipstick: 폰트 컴포넌트 스타일 추가

--- src/components/Text/Styles.tsx
+++ src/components/Text/Styles.tsx
@@ -16,53 +16,75 @@ const DefaultTextCSS = css<TextProps>`
 const Display1 = css`
   ${DefaultTextCSS}
 
-  font-size: ${({ theme }) => theme.fontSizes.xxl}px;
-  font-weight: ${({ theme }) => theme.fontWeights.regular};
-  line-height: 34px;
+  font-family: 'LeeSeoyun';
+  font-size: ${({ theme }) => theme.fontSizes.xxxl}px;
+  line-height: 125%;
+  letter-spacing: -1%;
 `;
 
 const Head1 = css`
   ${DefaultTextCSS}
 
-  font-size: ${({ theme }) => theme.fontSizes.md}px;
-  font-weight: ${({ theme }) => theme.fontWeights.regular};
-  line-height: 26px;
-  letter-spacing: -1%;
+  font-family: 'LeeSeoyun';
+  font-size: ${({ theme }) => theme.fontSizes.lg}px;
+  line-height: 130%;
+  letter-spacing: -2%;
 `;
 
 const Head2 = css`
   ${DefaultTextCSS}
 
+  font-family: 'LeeSeoyun';
   font-size: ${({ theme }) => theme.fontSizes.lg}px;
-  font-weight: ${({ theme }) => theme.fontWeights.regular};
-  line-height: 26px;
+  line-height: 130%;
+  letter-spacing: -2%;
 `;
 
 const Head3 = css`
   ${DefaultTextCSS}
 
-  font-size: ${({ theme }) => theme.fontSizes.xl}px;
-  font-weight: ${({ theme }) => theme.fontWeights.regular};
-  line-height: 30px;
+  font-family: 'LeeSeoyun';
+  font-size: ${({ theme }) => theme.fontSizes.mxld}px;
+  line-height: 130%;
   letter-spacing: -2%;
 `;
 
-const SubTitle1 = css`
+const Head4 = css`
   ${DefaultTextCSS}
 
-  font-family: 'NanumSquareRoundOTFB';
-  font-size: ${({ theme }) => theme.fontSizes.xs}px;
-  font-weight: ${({ theme }) => theme.fontWeights.bold};
-  line-height: 18.2px;
+  font-family: 'LeeSeoyun';
+  font-size: ${({ theme }) => theme.fontSizes.xxl}px;
+  line-height: 125%;
+  letter-spacing: -1%;
 `;
 
-const SubTitle2 = css`
+const SubHead1 = css`
+  ${DefaultTextCSS}
+
+  font-family: 'NanumSquareRoundOTFB';
+  font-size: ${({ theme }) => theme.fontSizes.xxs}px;
+  font-weight: ${({ theme }) => theme.fontWeights.bold};
+  line-height: 145%;
+  letter-spacing: -2%;
+`;
+
+const SubHead2 = css`
   ${DefaultTextCSS}
 
   font-family: 'NanumSquareRoundOTFB';
   font-size: ${({ theme }) => theme.fontSizes.xs}px;
   font-weight: ${({ theme }) => theme.fontWeights.bold};
-  line-height: 18.2px;
+  line-height: -2%;
+`;
+
+const SubHead3 = css`
+  ${DefaultTextCSS}
+
+  font-family: 'NanumSquareRoundOTFB';
+  font-size: ${({ theme }) => theme.fontSizes.sm}px;
+  font-weight: ${({ theme }) => theme.fontWeights.bold};
+  line-height: 145%;
+  letter-spacing: -2%;
 `;
 
 const Body1 = css`
@@ -72,6 +94,7 @@ const Body1 = css`
   font-size: ${({ theme }) => theme.fontSizes.xxs}px;
   font-weight: ${({ theme }) => theme.fontWeights.regular};
   line-height: 20px;
+  letter-spacing: -0.2%;
 `;
 
 const Body2 = css`
@@ -81,6 +104,7 @@ const Body2 = css`
   font-size: ${({ theme }) => theme.fontSizes.xs}px;
   font-weight: ${({ theme }) => theme.fontWeights.regular};
   line-height: 20px;
+  letter-spacing: -0.2%;
 `;
 
 const BodyText1 = css`
@@ -89,7 +113,18 @@ const BodyText1 = css`
   font-family: 'NotoSansKR-Regular';
   font-size: ${({ theme }) => theme.fontSizes.xs}px;
   font-weight: ${({ theme }) => theme.fontWeights.regular};
-  line-height: 26px;
+  line-height: 165%;
+  letter-spacing: -0.2%;
+`;
+
+const LetterText1 = css`
+  ${DefaultTextCSS}
+
+  font-family: 'NotoSansKR-Regular';
+  font-size: ${({ theme }) => theme.fontSizes.xs}px;
+  font-weight: ${({ theme }) => theme.fontWeights.regular};
+  line-height: 200%;
+  letter-spacing: -0.2%;
 `;
 
 const TextCSS = {
@@ -97,11 +132,14 @@ const TextCSS = {
   Head1,
   Head2,
   Head3,
-  SubTitle1,
-  SubTitle2,
+  Head4,
+  SubHead1,
+  SubHead2,
+  SubHead3,
   Body1,
   Body2,
   BodyText1,
+  LetterText1,
 };
 
 export default TextCSS;
